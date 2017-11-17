e89e0e72ac8db9aa61c673118812a6c19b5fcc4a
jandoant <jandoant@gmail.com>
2017-11-17 15:49:06 +0100

    adding empty time-picker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TimePickerComponent } from './time-picker/time-picker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimePickerComponent
   ],
   imports: [
     BrowserModule,
